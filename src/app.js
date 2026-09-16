ab8b05f866ae83873d1312c292bb8d3b7483b086
Awais <awaissajid421@gmail.com>
2026-09-16 23:36:15 +0500

    Server Creating

--- src/app.js
+++ src/app.js
@@ -3,11 +3,59 @@ const express=require('express');
 const app=express();
 
 
+// add a middleware
+app.use(express.json());
+
+const notes=[];
 
 
 
+// title , description
+// Post  /note
+
+app.post('/notes', (req, res) => {
+    console.log(req.body);
+    notes.push(req.body);
+
+    res.status(201).json({
+        message: "Note created successfully"
+    });
+}); 
+
+app.get('/notes', (req,res)=>{
+    res.status(200).json({
+        message : "notes fetched successfully",
+        notes:notes
+    })
+});
+
+app.delete('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+    delete notes[index]
+    res.status(200).json({
+        message: "note deleted successfully..."
+    })
+});
+
+app.patch('/notes/:index', (req,res)=>{
+
+    const index=req.params.index;
+    const description=req.body.description;
+
+    notes[index].description=description;
+
+    console.log(notes[index].description);
+
+    const title=req.body.title;
+    notes[index].title=title;
 
 
+
+    res.status(200).json({
+        message: "note updated successfully..."
+    })
+
+});
 
 
 module.exports=app;
